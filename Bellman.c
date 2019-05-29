67ba89f3aee672edcc62dcacee289390868e4739
klevm25 <ladohemmanuel@gmail.com>
2019-05-29 11:41:43 +0200

    Ajout de Bellmann.c "à corriger/tester"

--- Bellman.c
+++ Bellman.c
@@ -3,25 +3,25 @@
 #include <stdbool.h>
 #include "Bellman.h"
 
-void Bellman(struct TableSucc *H,double** Dist, int* pere,double* pi,int nbclient) {
+void Bellman(struct liste* H,double** Dist, int* pere,double* pi,int nbclient) {
     int r,i,j,k;
     // on choisit 0, qui correspond au dépôt comme racine
     r=0;
-    struct maillon* courant= H[r].Succ.tete;
+    struct maillon* courant= H[r].tete;
     for (k=0; k<=nbclient; k++) {
         pi[k] = 10000000;
         pere[k]=0;
     }
     pi[r]=0;
 
-    for(i=0; i < H[r].Succ.nbelem;i++) {
-        pi[courant->sommet +1] = Dist[r][courant->sommet +1];
+    for(i=0; i < H[r].nbelem;i++) {
+        pi[courant->sommet+1] = Dist[r][courant->sommet +1];
         pere[courant->sommet +1] = r;
         courant = courant->suivant;
     }
 
     for (i=1;i<nbclient+1;i++) {
-        for (j=0; j < H[i].Succ.nbelem;j++) {
+        for (j=0; j < H[i].nbelem;j++) {
             if (pi[i]+Dist[i][courant->sommet] < pi[courant->sommet]) {
                 pi[courant->sommet] = pi[i]+Dist[i][courant->sommet];
                 pere[courant->sommet] = i;
@@ -29,6 +29,4 @@ void Bellman(struct TableSucc *H,double** Dist, int* pere,double* pi,int nbclien
             }
         }
     }
-
-
 }
